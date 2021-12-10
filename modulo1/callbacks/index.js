21c7771cc40be8048878711dce1a3940b4e9363b
Raquelmms <raqueldemedeirosmartins@gmail.com>
2021-12-09 21:06:25 -0300

    desafios feitos

--- modulo1/callbacks/index.js
+++ modulo1/callbacks/index.js
@@ -171,3 +171,75 @@ const fraseComercialYpes = produtosYpe.map((produto) => {
 })
 
 console.log(fraseComercialYpes)
+
+
+// - 🏅  Desafios
+//     1. Dado o seguinte array de pokémons, realize as operações pedidas nos itens abaixo:
+        
+        const pokemons = [
+           { nome: "Bulbasaur", tipo: "grama" },
+           { nome: "Bellsprout", tipo: "grama" },
+           { nome: "Charmander", tipo: "fogo" },
+           { nome: "Vulpix", tipo: "fogo" },
+           { nome: "Squirtle", tipo: "água" },
+           { nome: "Psyduck", tipo: "água" },
+        ]
+        
+//         - ⭐  Resultados esperados em cada item
+            
+//             ```jsx
+//             // item A
+//             [
+//               "Bellsprout", 
+//               "Bulbasaur",
+//               "Charmander",
+//               "Psyduck", 
+//               "Squirtle", 
+//               "Vulpix"
+//             ]
+            
+//             // item B
+//             [
+//                "grama",
+//                "fogo",
+//                "água",
+//             ]
+//             ```
+            
+        
+//         a) Crie um novo array que contenha apenas o nome dos pokémons em **ordem alfabética**
+        
+            
+//             <aside>
+//             ⭐ Pesquise sobre o método `sort()`
+            
+//             </aside>
+   
+    const pokemonsOrdemAlfabetica = pokemons.map((pokemon) => {
+            const nome = pokemon.nome
+            return nome
+    })
+console.log(pokemonsOrdemAlfabetica.sort())
+  
+//         b) Crie um novo array apenas com os tipos dos pokémons, **sem repetição**
+        
+//         - 💡  Dica
+            
+//             <aside>
+//             ⭐ Existem várias maneiras de resolver esse exercício! Pesquise outras funções de arrays ou utilize loops juntamente com o filter 😄
+            
+//             </aside>
+
+const categoriasPokemons = pokemons.map((pokemon) => {
+    const categoriaPoke = pokemon.tipo
+    return categoriaPoke
+})
+
+
+const tiposPokeUnicos = new Set()
+
+categoriasPokemons.forEach((tipos) => {
+tiposPokeUnicos.add(tipos)
+})
+
+console.log([...tiposPokeUnicos.values()])
