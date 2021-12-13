756cd5cb4d3421f505595f837e6058dfe4c33802
Raquelmms <raqueldemedeirosmartins@gmail.com>
2021-12-13 17:09:44 -0300

    exercicio 13b feito

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -154,7 +154,10 @@ function retornaPessoasAutorizadas(pessoas) {
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+    const naoAutorizadas = pessoas.filter((pessoa) =>{
+        return !((pessoa.idade > 14 && pessoa.idade < 60) && pessoa.altura >= 1.5)
+    })
+    return naoAutorizadas
 }
 
 // EXERCÍCIO 14
