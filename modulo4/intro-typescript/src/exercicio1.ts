9050e95a33dde4557e9123c20bee5273ea6e6c8c
Raquelmms <raqueldemedeirosmartins@gmail.com>
2022-03-23 17:15:20 -0300

    exercicios ts

---
+++ modulo4/intro-typescript/src/exercicio1.ts
@@ -0,0 +1,19 @@
+const numero1 = Number(process.argv[2]);
+const numero2 = Number(process.argv[3]);
+const numero3 = Number(process.argv[4]);
+
+function checaTriangulo(a: number, b: number, c: number): string {
+  if (a !== b && b !== c) {
+    console.log('Escaleno')
+    return 'Escaleno';
+  } else if (a === b && b === c) {
+    console.log('Equilátero')
+    return 'Equilátero';
+  } else {
+    console.log('Isósceles')
+    return 'Isósceles';
+  }
+}
+
+checaTriangulo(numero1, numero2, numero3);
+
