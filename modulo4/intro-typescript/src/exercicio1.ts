1646cbcfbd98a48dab9cdbef39bc0b90631e7d0b
Raquelmms <raqueldemedeirosmartins@gmail.com>
2022-03-23 21:48:25 -0300

    finalização dos exercicios

--- modulo4/intro-typescript/src/exercicio1.ts
+++ modulo4/intro-typescript/src/exercicio1.ts
@@ -1,19 +1,19 @@
-const numero1 = Number(process.argv[2]);
-const numero2 = Number(process.argv[3]);
-const numero3 = Number(process.argv[4]);
+// const numero1 = Number(process.argv[2]);
+// const numero2 = Number(process.argv[3]);
+// const numero3 = Number(process.argv[4]);
 
-function checaTriangulo(a: number, b: number, c: number): string {
-  if (a !== b && b !== c) {
-    console.log('Escaleno')
-    return 'Escaleno';
-  } else if (a === b && b === c) {
-    console.log('Equilátero')
-    return 'Equilátero';
-  } else {
-    console.log('Isósceles')
-    return 'Isósceles';
-  }
-}
+// function checaTriangulo(a: number, b: number, c: number): string {
+//   if (a !== b && b !== c) {
+//     console.log('Escaleno')
+//     return 'Escaleno';
+//   } else if (a === b && b === c) {
+//     console.log('Equilátero')
+//     return 'Equilátero';
+//   } else {
+//     console.log('Isósceles')
+//     return 'Isósceles';
+//   }
+// }
 
-checaTriangulo(numero1, numero2, numero3);
+// checaTriangulo(numero1, numero2, numero3);
 
---
+++ modulo4/intro-typescript/src/exercicio3.ts
@@ -0,0 +1,9 @@
+// const year : number = Number(process.argv[2])
+
+// function checaAnoBissexto(ano : number):boolean {
+//   const cond1 = ano % 400 === 0
+//   const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
+//   return cond1 || cond2
+// }
+
+// checaAnoBissexto(year)
