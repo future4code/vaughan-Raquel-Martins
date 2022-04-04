1c00f0f58be0a4df836b3e9015a05fbd5602c113
Raquelmms <raqueldemedeirosmartins@gmail.com>
2022-04-03 21:20:01 -0300

    bills to pay

--- modulo4/sistema-bancario/src/data.ts
+++ modulo4/sistema-bancario/src/data.ts
@@ -1,15 +1,11 @@
-type Acquisition = {
-    price: number,
-    description: string,
-    date: string
-}
+
 
 type User = {
   name: string,
   cpf: number,
   birthDate: string,
   balance: number,
-  // extract: Acquisition[]
+  extract: any,
 }
 
 export const users: User[] = [
@@ -18,17 +14,20 @@ export const users: User[] = [
     cpf: 12098312399,
     birthDate: "09/01/2000",
     balance: 200,
+    extract: undefined
   },
   {
     name: "Amanda",
     cpf: 12098712392,
     birthDate: "30/08/1992",
     balance: 300,
+    extract: undefined
   },
   {
     name: "Fernando",
     cpf: 12398712321,
     birthDate: "23/02/1986",
     balance: 1000,
+    extract: undefined
   }
 ]
